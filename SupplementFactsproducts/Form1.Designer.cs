969f7e2ee754d3358e1966f98ddb23682cf2a7ec
TanVuong <tanvuong222@gmail.com>
2022-12-07 05:40:59 +0700

    Add project files.

---
+++ SupplementFactsproducts/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace SupplementFactsproducts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuManagement = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuCustomer = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuEmployee = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuProduct = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuInvoice = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuInvoiceDetail = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFind = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFindInvoice = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFindProduct = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFindCustomer = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuReport = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuInventory = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuTurnover = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuManagement,
+            this.mnuInvoice,
+            this.mnuFind,
+            this.mnuReport,
+            this.mnuHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.mnuExit.Size = new System.Drawing.Size(135, 22);
+            this.mnuExit.Text = "Exit";
+            // 
+            // mnuManagement
+            // 
+            this.mnuManagement.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuCustomer,
+            this.mnuEmployee,
+            this.mnuProduct});
+            this.mnuManagement.Name = "mnuManagement";
+            this.mnuManagement.Size = new System.Drawing.Size(90, 20);
+            this.mnuManagement.Text = "&Management";
+            // 
+            // mnuCustomer
+            // 
+            this.mnuCustomer.Name = "mnuCustomer";
+            this.mnuCustomer.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.C)));
+            this.mnuCustomer.Size = new System.Drawing.Size(180, 22);
+            this.mnuCustomer.Text = "Customer";
+            // 
+            // mnuEmployee
+            // 
+            this.mnuEmployee.Name = "mnuEmployee";
+            this.mnuEmployee.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.E)));
+            this.mnuEmployee.Size = new System.Drawing.Size(180, 22);
+            this.mnuEmployee.Text = "Employee";
+            // 
+            // mnuProduct
+            // 
+            this.mnuProduct.Name = "mnuProduct";
+            this.mnuProduct.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.P)));
+            this.mnuProduct.Size = new System.Drawing.Size(180, 22);
+            this.mnuProduct.Text = "Product";
+            // 
+            // mnuInvoice
+            // 
+            this.mnuInvoice.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuInvoiceDetail});
+            this.mnuInvoice.Name = "mnuInvoice";
+            this.mnuInvoice.Size = new System.Drawing.Size(57, 20);
+            this.mnuInvoice.Text = "&Invoice";
+            // 
+            // mnuInvoiceDetail
+            // 
+            this.mnuInvoiceDetail.Name = "mnuInvoiceDetail";
+            this.mnuInvoiceDetail.Size = new System.Drawing.Size(180, 22);
+            this.mnuInvoiceDetail.Text = "InvoiceDetail";
+            // 
+            // mnuFind
+            // 
+            this.mnuFind.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFindInvoice,
+            this.mnuFindProduct,
+            this.mnuFindCustomer});
+            this.mnuFind.Name = "mnuFind";
+            this.mnuFind.Size = new System.Drawing.Size(42, 20);
+            this.mnuFind.Text = "&Find";
+            // 
+            // mnuFindInvoice
+            // 
+            this.mnuFindInvoice.Name = "mnuFindInvoice";
+            this.mnuFindInvoice.Size = new System.Drawing.Size(180, 22);
+            this.mnuFindInvoice.Text = "Find_Invoice";
+            // 
+            // mnuFindProduct
+            // 
+            this.mnuFindProduct.Name = "mnuFindProduct";
+            this.mnuFindProduct.Size = new System.Drawing.Size(180, 22);
+            this.mnuFindProduct.Text = "Find_Product";
+            // 
+            // mnuFindCustomer
+            // 
+            this.mnuFindCustomer.Name = "mnuFindCustomer";
+            this.mnuFindCustomer.Size = new System.Drawing.Size(180, 22);
+            this.mnuFindCustomer.Text = "Find_Customer";
+            // 
+            // mnuReport
+            // 
+            this.mnuReport.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuInventory,
+            this.mnuTurnover});
+            this.mnuReport.Name = "mnuReport";
+            this.mnuReport.Size = new System.Drawing.Size(54, 20);
+            this.mnuReport.Text = "&Report";
+            // 
+            // mnuInventory
+            // 
+            this.mnuInventory.Name = "mnuInventory";
+            this.mnuInventory.Size = new System.Drawing.Size(180, 22);
+            this.mnuInventory.Text = "Inventory";
+            this.mnuInventory.Click += new System.EventHandler(this.inventoryToolStripMenuItem_Click);
+            // 
+            // mnuTurnover
+            // 
+            this.mnuTurnover.Name = "mnuTurnover";
+            this.mnuTurnover.Size = new System.Drawing.Size(180, 22);
+            this.mnuTurnover.Text = "Turnover";
+            // 
+            // mnuHelp
+            // 
+            this.mnuHelp.Name = "mnuHelp";
+            this.mnuHelp.Size = new System.Drawing.Size(44, 20);
+            this.mnuHelp.Text = "&Help";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Supplement Facts products";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.ToolStripMenuItem mnuManagement;
+        private System.Windows.Forms.ToolStripMenuItem mnuCustomer;
+        private System.Windows.Forms.ToolStripMenuItem mnuEmployee;
+        private System.Windows.Forms.ToolStripMenuItem mnuProduct;
+        private System.Windows.Forms.ToolStripMenuItem mnuInvoice;
+        private System.Windows.Forms.ToolStripMenuItem mnuInvoiceDetail;
+        private System.Windows.Forms.ToolStripMenuItem mnuFind;
+        private System.Windows.Forms.ToolStripMenuItem mnuFindInvoice;
+        private System.Windows.Forms.ToolStripMenuItem mnuFindProduct;
+        private System.Windows.Forms.ToolStripMenuItem mnuFindCustomer;
+        private System.Windows.Forms.ToolStripMenuItem mnuReport;
+        private System.Windows.Forms.ToolStripMenuItem mnuInventory;
+        private System.Windows.Forms.ToolStripMenuItem mnuTurnover;
+        private System.Windows.Forms.ToolStripMenuItem mnuHelp;
+    }
+}
+
---
+++ SupplementFactsproducts/FormEmployee.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace SupplementFactsproducts
+{
+    partial class FormEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_EmployeeID = new System.Windows.Forms.TextBox();
+            this.txt_EmployeeName = new System.Windows.Forms.TextBox();
+            this.CheckMale = new System.Windows.Forms.CheckBox();
+            this.CheckFemale = new System.Windows.Forms.CheckBox();
+            this.dateTimeDayOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Controls.Add(this.btnSkip);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.btnEdit);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 350);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dateTimeDayOfBirth);
+            this.panel2.Controls.Add(this.CheckFemale);
+            this.panel2.Controls.Add(this.CheckMale);
+            this.panel2.Controls.Add(this.txt_EmployeeName);
+            this.panel2.Controls.Add(this.txt_EmployeeID);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 144);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 144);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 206);
+            this.panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(266, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LIST OF EMPLOYEE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Employee ID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Employee Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(470, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(470, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Date of birth";
+            // 
+            // txt_EmployeeID
+            // 
+            this.txt_EmployeeID.Location = new System.Drawing.Point(176, 52);
+            this.txt_EmployeeID.Name = "txt_EmployeeID";
+            this.txt_EmployeeID.Size = new System.Drawing.Size(163, 20);
+            this.txt_EmployeeID.TabIndex = 4;
+            // 
+            // txt_EmployeeName
+            // 
+            this.txt_EmployeeName.Location = new System.Drawing.Point(176, 113);
+            this.txt_EmployeeName.Name = "txt_EmployeeName";
+            this.txt_EmployeeName.Size = new System.Drawing.Size(163, 20);
+            this.txt_EmployeeName.TabIndex = 5;
+            // 
+            // CheckMale
+            // 
+            this.CheckMale.AutoSize = true;
+            this.CheckMale.Location = new System.Drawing.Point(518, 55);
+            this.CheckMale.Name = "CheckMale";
+            this.CheckMale.Size = new System.Drawing.Size(49, 17);
+            this.CheckMale.TabIndex = 6;
+            this.CheckMale.Text = "Male";
+            this.CheckMale.UseVisualStyleBackColor = true;
+            // 
+            // CheckFemale
+            // 
+            this.CheckFemale.AutoSize = true;
+            this.CheckFemale.Location = new System.Drawing.Point(573, 55);
+            this.CheckFemale.Name = "CheckFemale";
+            this.CheckFemale.Size = new System.Drawing.Size(60, 17);
+            this.CheckFemale.TabIndex = 7;
+            this.CheckFemale.Text = "Female";
+            this.CheckFemale.UseVisualStyleBackColor = true;
+            // 
+            // dateTimeDayOfBirth
+            // 
+            this.dateTimeDayOfBirth.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimeDayOfBirth.Location = new System.Drawing.Point(541, 117);
+            this.dateTimeDayOfBirth.Name = "dateTimeDayOfBirth";
+            this.dateTimeDayOfBirth.Size = new System.Drawing.Size(200, 20);
+            this.dateTimeDayOfBirth.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(25, 26);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(125, 47);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(150, 26);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(125, 47);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "&Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(275, 26);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(125, 47);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "&Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(400, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 47);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "&Save";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnSkip
+            // 
+            this.btnSkip.Location = new System.Drawing.Point(525, 26);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(125, 47);
+            this.btnSkip.TabIndex = 9;
+            this.btnSkip.Text = "Skip";
+            this.btnSkip.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(650, 26);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(125, 47);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // FormEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormEmployee";
+            this.Text = "FormEmployee";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSkip;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DateTimePicker dateTimeDayOfBirth;
+        private System.Windows.Forms.CheckBox CheckFemale;
+        private System.Windows.Forms.CheckBox CheckMale;
+        private System.Windows.Forms.TextBox txt_EmployeeName;
+        private System.Windows.Forms.TextBox txt_EmployeeID;
+    }
+}
